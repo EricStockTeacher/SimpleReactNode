6543e73ac895fbf27c1362b26832fedf8e0cb673
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-02-20 14:41:41 -0330

    Initial Project

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/message', (req, res) => {
+    res.send("Hello Class");
+})
+
+app.listen(8000, () => console.log(`Running on port 8000`));
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <h2>Hello Everyone</h2>
+  );
+}
+
+export default App;
